eaa1c036b120cdf4700bc0ff61b01bf1d508bad9
Hoàng <hoang7bvo@gmail.com>
2024-10-28 21:24:10 +0700

    update

--- app/Models/ToCao.php
+++ app/Models/ToCao.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class ToCao extends Model
 {
     use HasFactory;
+
+    protected $table = 'to_caos';
+    protected $fillable = [
+        'id_nguoi_dung',
+        'id_player',
+        'noi_dung_to_cao',
+        'trang_thai'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'id_nguoi_dung');
+    }
+
+    public function player()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'id_player');
+    }
 }
