47908cf2d2a20fa3ad51c8690faeaf482fc74fda
Hoàng <hoang7bvo@gmail.com>
2024-10-28 21:25:03 +0700

    Hoang

--- app/Http/Controllers/ToCaoController.php
+++ app/Http/Controllers/ToCaoController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ToCao;
 
 class ToCaoController extends Controller
 {
     //
+    public function index()
+    {
+        $complaints = ToCao::with(['user', 'player'])->get();
+        return view('admin.tocaos.index', compact('complaints'));
+    }
 }
