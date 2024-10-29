533a8eabb8996813ca270742e1558edc3febea84
djtmeconchoo11 <khangtbph39573@fpt.edu.vn>
2024-10-29 16:56:13 +0700

    list2

--- app/Http/Controllers/TaiKhoanController.php
+++ app/Http/Controllers/TaiKhoanController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\TaiKhoan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TaiKhoanController extends Controller
 {
+    const path_upload = "public/uploads/tai_khoans";
     public function index(Request $request)
     {
         $taikhoans = TaiKhoan::all();
@@ -16,5 +18,30 @@ class TaiKhoanController extends Controller
     {
 
         return view("admin.taikhoans.create");
+    }
+    public function store(Request $request)
+    {
+        // Thêm validation
+        $request->validate([
+            'ten' => 'required|string|max:255',
+            'ngay_sinh' => 'required|date',
+            'biet_danh' => 'required|string|max:255',
+            'gioi_tinh' => 'required|in:Nam,Nữ',
+            'email' => 'required|email|unique:users,email',
+            'sdt' => 'required|numeric|digits_between:10,15',
+            'cccd' => 'required|numeric|digits:12|unique:users,cccd',
+            'mat_khau' => 'required|string|min:8',
+            'so_du' => 'required|numeric|min:0',
+            'anh_dai_dien' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'bi_cam' => 'required|in:1,2',
+        ]);
+
+        // Xử lý file ảnh và lưu vào database
+        $anh_dai_diens = $request->except("anh_dai_dien");
+        if ($request->hasFile("anh_dai_dien")) {
+            $anh_dai_diens['anh_dai_dien'] = Storage::put(self::path_upload, $request->file('anh_dai_dien'));
+        }
+        $data = TaiKhoan::create($anh_dai_diens);
+        return redirect()->route('index')->with('success', 'Thêm tài khoản thành công!');
     }
 }
