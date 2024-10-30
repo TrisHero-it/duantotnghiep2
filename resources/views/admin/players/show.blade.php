361a947b5d7b8c752af884b5e55804e70480d702
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-10-30 16:26:00 +0700

    them giao dien 5

--- resources/views/admin/players/show.blade.php
+++ resources/views/admin/players/show.blade.php
@@ -112,6 +112,7 @@
     <div class="col-xl-12"></div>
     <div class="col-xl-3 col-md-6"></div>
     <div class="col-xl-3 col-md-6"></div>
+    <div class="col-xl-3 col-md-6"></div>
 </div>
 
 @endsection
