ce6cefe48abcd463131b71598376d3d54a8c4418
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-10-30 16:31:21 +0700

    sua giao dien 2

--- resources/views/admin/players/show.blade.php
+++ resources/views/admin/players/show.blade.php
@@ -111,23 +111,39 @@
     <div class="col-xl-8"></div>
     <div class="col-xl-12"></div>
     <div class="col-xl-3 col-md-6">
-                                <div class="card prod-p-card bg-c-red">
-                                    <div class="card-body">
-                                        <div class="row align-items-center m-b-25">
-                                            <div class="col">
-                                                <h6 class="m-b-5 text-white">Total Profit</h6>
-                                                <h3 class="m-b-0 text-white">$1,783</h3>
-                                            </div>
-                                            <div class="col-auto">
-                                                <i class="fas fa-money-bill-alt text-c-red f-18"></i>
-                                            </div>
-                                        </div>
-                                        <p class="m-b-0 text-white"><span
-                                                class="label label-danger m-r-10">+11%</span>From Previous Month</p>
-                                    </div>
-                                </div>
-                            </div>
-    <div class="col-xl-3 col-md-6"></div>
+        <div class="card prod-p-card bg-c-red">
+            <div class="card-body">
+                <div class="row align-items-center m-b-25">
+                    <div class="col">
+                        <h6 class="m-b-5 text-white">Total Profit</h6>
+                        <h3 class="m-b-0 text-white">$1,783</h3>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-money-bill-alt text-c-red f-18"></i>
+                    </div>
+                </div>
+                <p class="m-b-0 text-white"><span
+                        class="label label-danger m-r-10">+11%</span>From Previous Month</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+        <div class="card prod-p-card bg-c-blue">
+            <div class="card-body">
+                <div class="row align-items-center m-b-25">
+                    <div class="col">
+                        <h6 class="m-b-5 text-white">Total Orders</h6>
+                        <h3 class="m-b-0 text-white">15,830</h3>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-database text-c-blue f-18"></i>
+                    </div>
+                </div>
+                <p class="m-b-0 text-white"><span
+                        class="label label-primary m-r-10">+12%</span>From Previous Month</p>
+            </div>
+        </div>
+    </div>
     <div class="col-xl-3 col-md-6"></div>
     <div class="col-xl-3 col-md-6"></div>
 </div>
