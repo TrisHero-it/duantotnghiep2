8bbd5e83493b3d926b01a345219e68aab0526055
trishero <trinmph36953@fpt.edu.vn>
2024-11-01 20:15:04 +0700

    ben8

--- resources/views/admin/binh-luans/index.blade.php
+++ resources/views/admin/binh-luans/index.blade.php
@@ -10,6 +10,9 @@
 <link rel="stylesheet" href="{{asset('assets/plugins/ratting/css/fontawesome-stars.css')}}">
 <link rel="stylesheet" href="{{asset('assets/plugins/ratting/css/fontawesome-stars-o.css')}}">
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
 <Style>
     .stars .star {
         font-size: 24px;
