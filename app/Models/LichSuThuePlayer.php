3d59d20988bd6932854928c035106649ab4ab4b7
Hoàng <hoang7bvo@gmail.com>
2024-11-02 12:33:32 +0700

    Hoang

--- app/Models/LichSuThuePlayer.php
+++ app/Models/LichSuThuePlayer.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class LichSuThuePlayer extends Model
 {
     use HasFactory;
+    public $fillable = [
+        "tai_khoan_id",
+        "player_id",
+        "gia_player",
+        "gio_thue",
+        "trang_thai_thue",
+
+    ];
+
+    public function taiKhoan()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'tai_khoan_id');
+    }
+
+    public function player()
+    {
+        return $this->hasMany(Player::class, 'player_id');
+    }
+
+    public function theoDoiPlayer()
+    {
+        return $this->hasOne(TheoDoiPlayer::class, 'player_id');
+    }
 }
