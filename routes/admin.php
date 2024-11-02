f52115a2744ba8f88d647663b07199c3ed34468f
Hoàng <hoang7bvo@gmail.com>
2024-11-02 12:41:21 +0700

    Hoang

--- routes/admin.php
+++ routes/admin.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('admin.layouts.app');
 });
 
-<<<<<<< HEAD
+
 
 Route::get('/to-caos', [ToCaoController::class, 'index'])->name('tocao.index');
 Route::delete('/to-caos/{complaint}', [ToCaoController::class, 'destroy'])->name('tocaos.destroy');
@@ -29,7 +29,7 @@ Route::get('/to-caos/add', [ToCaoController::class, 'create'])->name('tocao.add'
 Route::post('/to-caos/add', [ToCaoController::class, 'store'])->name('tocao.store');
 Route::get('/to-caos/{complaint}', [ToCaoController::class, 'show'])->name('tocao.show');
 
-=======
+
 Route::get('/dangtins', [DangTinController::class, 'index'])->name('dangtins.index');
 Route::get('/dangtins/create', [DangTinController::class, 'create'])->name('dangtins.create');
 Route::post('/dangtins', [DangTinController::class, 'store'])->name('dangtins.store');
@@ -42,7 +42,6 @@ Route::get('/phuongthucthanhtoans/{id}/edit', [PhuongThucThanhToanController::cl
 Route::put('/phuongthucthanhtoans/{id}', [PhuongThucThanhToanController::class, 'update'])->name('phuongthucthanhtoans.update');
 Route::put('/phuongthucthanhtoans/{id}/update-status', [PhuongThucThanhToanController::class, 'updateStatus'])->name('phuongthucthanhtoans.update-status');
 Route::delete('/phuongthucthanhtoans/{id}', [PhuongThucThanhToanController::class, 'destroy'])->name('phuongthucthanhtoans.destroy');
->>>>>>> 7dab740760a36ee85a91e0c531a6321e954bc672
 
 Route::resource('players', PlayerController::class);
 Route::get('/bieu-do-duong', [PlayerController::class, 'bieudo']);
